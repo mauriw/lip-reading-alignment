ff36a4e3cd41cf080580d15a05c5b7f704955a6d
Mauricio Wulfovich <mauriw@stanford.edu>
2021-06-02 16:42:45 -0700

    Load all data

--- constants.py
+++ constants.py
@@ -1,2 +1,7 @@
+from pathlib import Path
+
 EMBED_LEN = 512
 SEQ_LEN = 32
+DATA_PATH = Path.cwd() / 'data' / 'embeddings'
+EMBED_SUFFIX = '_final_embeddings.npy'
+LABELS_SUFFIX = '_embedding_labels.npy'
--- utils.py
+++ utils.py
@@ -1,13 +1,17 @@
 import numpy as np
-from pathlib import Path
 
-# TODO:
-#   - separate embeddings and labels into 2 folders
-#   - iterate over both, and return concatenated embeddings and labels
+import constants
 
 def load_data(vid):
-    x_path = Path.cwd() / 'data' / 'embeddings' / (vid + '_final_embeddings.npy')
-    y_path = Path.cwd() / 'data' / 'embeddings' / (vid + '_embedding_labels.npy')
+    x_path = constants.DATA_PATH / (vid + constants.EMBED_SUFFIX)
+    y_path = constants.DATA_PATH / (vid + constants.LABELS_SUFFIX)
     x = np.load(x_path)
     y = np.load(y_path)
     return x, y
+
+def load_all_data():
+    vids = ['5l4cA8zSreQ','pNttjJUtkA4', 'GjYt5uQPu8o', 'WFImhWa5oUw', 'urKhVssiygA', 'CLWRclarri0', 'j7RsRnYlz7I']
+    embeddings, labels = zip(*[load_data(vid) for vid in vids])
+    embeddings = np.concatenate(embeddings, axis=0)
+    labels = np.concatenate(labels, axis=0)
+    return embeddings, labels
